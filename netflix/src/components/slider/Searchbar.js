460590e6835a57ca76a66d4d8bfb83fa0d4bce9b
RamuLincoln <Ramu.Valliappan@lincolnuni.ac.nz>
2024-03-08 11:38:50 +1300

    search

--- netflix/src/components/slider/Searchbar.js
+++ netflix/src/components/slider/Searchbar.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
 import { movieData } from "../../Data";
 import Search from '../card/Search';
 
